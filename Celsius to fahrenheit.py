6192edb468dfcac2d9966fa1913f201605c3aa35
ArifRahaman <115584161+ArifRahaman@users.noreply.github.com>
2022-10-20 09:25:22 +0530

    Add files via upload

---
+++ Celsius to fahrenheit.py
@@ -0,0 +1,7 @@
+print("Enter the celsius value: ")
+n1=int(input())
+print("The fahrenheit value is : ", n1*1.8+32)
+# C =1.repl.it,  2.vs code
+#c++ = 1.repl.it,  2.vs code
+#javascript= 1. repl.it, 2......
+# Python= 1. Pycharm ,  2.  VS Code(60)  repl.it(40)
